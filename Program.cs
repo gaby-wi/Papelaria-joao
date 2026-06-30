a3a5161dbdd618cfbf989ac3ef3cee33e985e1ff
Yasmim Lima de Azevedo <yasmimlimadeazevedolima@gmail.com>
2026-06-30 08:40:26 -0400

    Explicação breve do Codigo
    Commit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 using AppPapelaria1.Components;
 
+// Cria e configura a aplicação Blazor
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adiciona os componentes Razor e habilita a interatividade
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Cria a aplicação
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura tratamento de erros em ambiente de produção
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -16,6 +18,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// Proteção contra ataques CSRF
 app.UseHttpsRedirection();
 
 
@@ -25,4 +28,5 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+// Inicializa a aplicação
 app.Run();
