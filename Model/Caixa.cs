b235b4486d2994750e839d15fa814083cc73dc35
Gaby <araujo.g@estudante.ifro.edu.br>
2026-09-22 11:26:18 -0400

    commit

--- Model/Caixa.cs
+++ Model/Caixa.cs
@@ -1,12 +1,16 @@
-﻿namespace AppPapelaria1.Model
+﻿namespace AppPapelaria1.Model;
+
+public class Caixa
 {
-    public class Caixa
-    {
-        public int Id { get; set; }
-        public DateTime DataAbertura { get; set; }
-        public DateTime DataFechamento { get; set; }
-        public float ValorInicial { get; set; }
-        public float ValorFinal { get; set; }
-        public string Status { get; set; } = string.Empty;
-    }
+    public int Id { get; set; }
+
+    public DateTime DataAbertura { get; set; }
+
+    public DateTime? DataFechamento { get; set; }
+
+    public float ValorInicial { get; set; }
+
+    public float? ValorFinal { get; set; }
+
+    public string Status { get; set; } = "";
 }
--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@ using AppPapelaria1;
 using AppPapelaria1.Config;
 using AppPapelaria1.DAO;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-
 builder.Services.AddScoped<Conexao>();
+builder.Services.AddScoped<CaixaDAO>();
 builder.Services.AddScoped<ProcessoDAO>();
 builder.Services.AddScoped<FornecedorDAO>();
 
@@ -23,11 +23,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAntiforgery();
+
 app.MapStaticAssets();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-
 app.Run();
-
