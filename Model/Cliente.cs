79cf3f0498c4e340f8fc4d47c259711b84374e55
Brayon-235 <167538021+Brayon-235@users.noreply.github.com>
2026-09-22 10:35:12 -0400

    testando

---
+++ Model/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace AppPapelaria1.Model
+{
+    public class Cliente
+    {
+        public int IdCli { get; set; }
+
+        public string NomeCli { get; set; } 
+
+        public string CpfCli { get; set; } 
+
+        public string TelefoneCli { get; set; } 
+
+        public string EmailCli { get; set; } 
+
+        public string EnderecoCli { get; set; } 
+    }
+}
