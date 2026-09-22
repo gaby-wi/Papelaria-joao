82ce2b105b4154f5d79ea11a04c2d504811d5539
Gaby <araujo.g@estudante.ifro.edu.br>
2026-09-22 10:03:12 -0400

    commit

--- Program.cs
+++ Program.cs
@@ -1,34 +1,18 @@
 using AppPapelaria1;
-<<<<<<< HEAD
-using AppPapelaria1.Components;
-using AppPapelaria1.Components.Pages.DAO;
-=======
->>>>>>> 65c121a0acc9cea91570d8a6d72bb9a9be6442dd
 using AppPapelaria1.Config;
 using AppPapelaria1.DAO;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona serviços do Blazor
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-<<<<<<< HEAD
-// Registra a Conexão e os DAOs
-builder.Services.AddScoped<Conexao>();
-builder.Services.AddScoped<CaixaDAO>();
-builder.Services.AddScoped<FornecedorDAO>();
-builder.Services.AddScoped<processoDAO>();
 
-=======
-////configuração da conexão com o banco de dados MYSQL
 builder.Services.AddScoped<Conexao>();
 builder.Services.AddScoped<ProcessoDAO>();
 builder.Services.AddScoped<FornecedorDAO>();
 
-
-// Cria a aplicação
->>>>>>> 65c121a0acc9cea91570d8a6d72bb9a9be6442dd
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -44,11 +28,6 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-<<<<<<< HEAD
-app.Run();
-=======
-// Inicializa a aplicação
+
 app.Run();
 
-
->>>>>>> 65c121a0acc9cea91570d8a6d72bb9a9be6442dd
