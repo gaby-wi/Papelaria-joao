c1568188430b054305cd37dd8f83e519c8af5300
yasminpereirareis29-star <yasminpereirareis29@gmail.com>
2026-09-22 09:16:44 -0400

    commit

--- DAO/FornecedorDAO.cs
+++ DAO/FornecedorDAO.cs
@@ -1,12 +1,11 @@
-﻿using AppPapelaria1.Components.Pages;
+﻿
 using AppPapelaria1.Config;
 using AppPapelaria1.Model;
-using static Mysqlx.Expect.Open.Types.Condition.Types;
+namespace AppPapelaria1.DAO;
 
 
 
-namespace AppPapelaria1.DAO
-{
+
     public class FornecedorDAO
     {
         private readonly Conexao _conexao;
@@ -58,4 +57,4 @@ namespace AppPapelaria1.DAO
             }
         }
     }
-}
+
